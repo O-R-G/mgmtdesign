d88fca8aee7abc717eeaf26469dc57b63dbd19ff
Eric Li <ericyoungli@gmail.com>
2018-10-03 23:06:09 -0400

    add correct image support

--- views/studio.php
+++ views/studio.php
@@ -29,10 +29,10 @@
 <div class = "bodyContainer mobileStudioBody">
   <?php foreach($oo->media($uu->id) as $media): ?>
     <div class = "imagesubContainer">
-      <img src="<?= m_url($media);?>">
+      <img src="<?= m_url($media);?>" style="width: 100%; height: 100%">
       <?php if($media['caption']): ?>
         <div class="imageCaptionContainer Caption">
-          <?= $media['caption']; ?>
+          <?= strip_tags($media['caption']); ?>
         </div>
       <?php endif; ?>
     </div>
