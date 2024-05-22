b455346bc389de73207efef19247c3e6f56e5d7c
User Mgmtdesign <mgmtdesign@www2.g18.pair.com>
2024-05-22 12:20:23 -0400

    adjust index to work for both mgmtdesign and the-copy

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if ($uri[1] == "contact") {
   require_once("views/contact.php");
 } else if ($uri[1] == "about") {
   require_once("views/studio.php");
-} else if ($uri[1] == "projects" || $uri[1] == "research") {
+} else if ($uri[1] == "projects" || $uri[1] == "research" || $uri[1] == "work") {
   require_once("views/work.php");
 } else {
   require_once("views/home.php");
